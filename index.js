5bdd986f256cd35931a8a2a05adc4c817567038a
muralin250499 <muralin25499@gmail.com>
2020-03-04 17:18:32 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-//var person=prompt("please Enter your name");
-//var start=prompt("Start index");
+var person=prompt("please Enter your name");
+var start=prompt("Start index");
 while(isNaN(start)==true)
 {
-  //alert("Enter valid value");
-  //var start=prompt("start index");
+  alert("Enter valid value");
+  var start=prompt("start index");
 }
-//var end=prompt("end index");
+var end=prompt("end index");  
 while(isNaN(end)==true)
 {
-  //alert("Enter valid value");
-  //var end=prompt("End index");
+  alert("Enter valid value");
+  var end=prompt("End index");
 }
-//alert(person.slice(start,end));
+alert(person.slice(start,end));
